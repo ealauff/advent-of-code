97738a135ab6b19b9260d9ba0c3779e7cc815ca9
ealauff <57789576+ealauff@users.noreply.github.com>
2021-12-07 14:37:20 -0500

    Update 02-p1.py

--- 02/02-p1.py
+++ 02/02-p1.py
@@ -28,7 +28,7 @@
 
 # Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
 
-# Your puzzle answer was 1693300.
+# Your puzzle answer was XXXXXXX.
 
 ########################################################################################################################################################################################################################################################
 
