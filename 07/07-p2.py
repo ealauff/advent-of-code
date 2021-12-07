73bae8e0e90055b2a323826d0fc0f562960c11e5
ealauff <57789576+ealauff@users.noreply.github.com>
2021-12-07 14:56:59 -0500

    Update 07-p2.py

--- 07/07-p2.py
+++ 07/07-p2.py
@@ -1,9 +1,12 @@
 # --- Part Two ---
 # The crabs don't seem interested in your proposed solution. Perhaps you misunderstand crab engineering?
 
-# As it turns out, crab submarine engines don't burn fuel at a constant rate. Instead, each change of 1 step in horizontal position costs 1 more unit of fuel than the last: the first step costs 1, the second step costs 2, the third step costs 3, and so on.
+# As it turns out, crab submarine engines don't burn fuel at a constant rate.
+# Instead, each change of 1 step in horizontal position costs 1 more unit of fuel than the last:
+# the first step costs 1, the second step costs 2, the third step costs 3, and so on.
 
-# As each crab moves, moving further becomes more expensive. This changes the best horizontal position to align them all on; in the example above, this becomes 5:
+# As each crab moves, moving further becomes more expensive.
+# This changes the best horizontal position to align them all on; in the example above, this becomes 5:
 
 # Move from 16 to 5: 66 fuel
 # Move from 1 to 5: 10 fuel
@@ -15,13 +18,15 @@
 # Move from 1 to 5: 10 fuel
 # Move from 2 to 5: 6 fuel
 # Move from 14 to 5: 45 fuel
-# This costs a total of 168 fuel. This is the new cheapest possible outcome; the old alignment position (2) now costs 206 fuel instead.
+# This costs a total of 168 fuel.
+# This is the new cheapest possible outcome; the old alignment position (2) now costs 206 fuel instead.
 
-# Determine the horizontal position that the crabs can align to using the least fuel possible so they can make you an escape route! How much fuel must they spend to align to that position?
+# Determine the horizontal position that the crabs can align to using the least fuel possible so they can make you an escape route!
+# How much fuel must they spend to align to that position?
 
 # Your puzzle answer was XXXXXXXX.
 
-########################################################################################################################################################################################################################################################
+###########################################################################################################################################################################
 
 import pathlib
 
