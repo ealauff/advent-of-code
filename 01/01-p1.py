f6d2d4f2b33f98d1ac7694430afd6766c49c333f
ealauff <57789576+ealauff@users.noreply.github.com>
2021-12-07 14:42:18 -0500

    Update 01-p1.py

--- 01/01-p1.py
+++ 01/01-p1.py
@@ -1,4 +1,6 @@
-# As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea floor. On a small screen, the sonar sweep report (your puzzle input) appears: each line is a measurement of the sea floor depth as the sweep looks further and further away from the submarine.
+# As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea floor. 
+# On a small screen, the sonar sweep report (your puzzle input) appears: 
+# each line is a measurement of the sea floor depth as the sweep looks further and further away from the submarine.
 
 # For example, suppose you had the following report:
 
@@ -15,9 +17,11 @@
 
 # This report indicates that, scanning outward from the submarine, the sonar sweep found depths of 199, 200, 208, 210, and so on.
 
-# The first order of business is to figure out how quickly the depth increases, just so you know what you're dealing with - you never know if the keys will get carried into deeper water by an ocean current or a fish or something.
+# The first order of business is to figure out how quickly the depth increases, just so you know what you're dealing with - 
+# you never know if the keys will get carried into deeper water by an ocean current or a fish or something.
 
-# To do this, count the number of times a depth measurement increases from the previous measurement. (There is no measurement before the first measurement.) In the example above, the changes are as follows:
+# To do this, count the number of times a depth measurement increases from the previous measurement. 
+# (There is no measurement before the first measurement.) In the example above, the changes are as follows:
 
 # 199 (N/A - no previous measurement)
 # 200 (increased)
@@ -36,7 +40,7 @@
 
 # Your puzzle answer was XXXX.
 
-########################################################################################################################################################################################################################################################
+###########################################################################################################################################################################
 
 import pathlib
 
