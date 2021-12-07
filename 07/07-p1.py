aca26fa7858ba90130d1b35e84e989ef5c761c3f
ealauff <57789576+ealauff@users.noreply.github.com>
2021-12-07 14:40:48 -0500

    Update 07-p1.py

--- 07/07-p1.py
+++ 07/07-p1.py
@@ -30,7 +30,7 @@
 
 # Determine the horizontal position that the crabs can align to using the least fuel possible. How much fuel must they spend to align to that position?
 
-# Your puzzle answer was 342730.
+# Your puzzle answer was XXXXXX.
 
 ########################################################################################################################################################################################################################################################
 
